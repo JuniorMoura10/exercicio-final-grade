aec379c6d4ccfdbe51201478421d027910601113
Júnior Moura <geraldomartins10@gmail.com>
2022-08-24 12:45:55 -0300

    exercício final grade

---
+++ src/entities/Student.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Student {
+
+    public String name;
+    public double score1;
+    public double score2;
+    public double score3;
+
+    public double finalScore(){
+        return score1 + score2 + score3;
+    }
+
+    public double missingPoints(){
+        if (finalScore() < 60){
+            return 60 - finalScore();
+        }else{
+            return 0.0;
+        }
+    }
+}
